b84642f52add74fa515ac46ea0b1930167ce1665
Eric <rovx15@gmail.com>
2017-02-22 22:53:51 +0100

    FIN

--- src/activitat3/TCPCliente.java
+++ src/activitat3/TCPCliente.java
@@ -7,34 +7,43 @@ import java.io.InputStreamReader;
 import java.net.Socket;
 import javax.swing.JOptionPane;
 
-
 public class TCPCliente {
+    //Declarem el HOST i el PORT
     static String HOST = "localhost";
     static int PORT = 5487;
-    
-
-    public TCPCliente(String host, int port) throws IOException{
+    //Inicialitzem el DataOutput i el BufferReader
+    DataOutputStream outToServer;
+    BufferedReader bf;
+    /**
+     * Constructor que li arriba un host i un port.
+     * S'inicialitza el Socket, s'escriu i s'envia una frase al server.
+     * @param host
+     * @param port
+     * @throws IOException 
+     */
+    public TCPCliente(String host, int port) throws IOException {
+        //Inicializa el Socket
         Socket ss = new Socket(host, port);
-        
-        DataOutputStream outToServer = new DataOutputStream(ss.getOutputStream());
-        BufferedReader bf = new BufferedReader (new InputStreamReader(ss.getInputStream()));
-                        
-        outToServer.writeBytes("Hola  \n");        
-        
+        //Inicializamos el DataOutput i el BufferedReader
+        outToServer = new DataOutputStream(ss.getOutputStream());
+        bf = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+        //Escribimos en el DataOutput una frase
+        outToServer.writeBytes("Hola  \n");
+        //Leemos la linea que nos llega del BufferedReader
         String cadena = bf.readLine();
+        //La mostramos
         System.out.println(cadena);
+        //La mostramos en un Pane
         JOptionPane.showMessageDialog(null, cadena);
-        
+
         outToServer.close();
         bf.close();
         ss.close();
     }
-    
-   public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException {
         new TCPCliente(HOST, PORT);
-        
+
     }
-    
-    
-    
+
 }
--- src/activitat3/TCPServidor.java
+++ src/activitat3/TCPServidor.java
@@ -9,23 +9,37 @@ import java.net.Socket;
 import javax.swing.JOptionPane;
 
 public class TCPServidor {
+    //Inicialitzem el port, ServerSocket, DataOutput i BufferedReader
     static int PORT = 5487;
-
+     ServerSocket serverSocket;
+     DataOutputStream outToClient;
+     BufferedReader bf;
+     /**
+      * El constructor li arriba un port.
+      * Inicialitza el ServerSocket i li arriba del Client una frase que llegeix.
+      * A aquesta cadena li inclueix un string que hem escrit nosaltres i el mostra en un Pane.
+      * @param port
+      * @throws IOException 
+      */
     public TCPServidor(int port) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(port);
-
+        //Inicialitzem el ServerSocket
+       serverSocket = new ServerSocket(port);
+       //Realitzem un bucle
         while (true) {
+            //Inicialitzem un Socket
             Socket ss = serverSocket.accept();
-
-            DataOutputStream outToClient = new DataOutputStream(ss.getOutputStream());
-            BufferedReader bf = new BufferedReader(new InputStreamReader(ss.getInputStream()));
-
+            //Inicialitzem DataOutput i BufferedReader
+            outToClient = new DataOutputStream(ss.getOutputStream());
+            bf = new BufferedReader(new InputStreamReader(ss.getInputStream()));
+            //Donem valor a un String amb lo que llegim a la linea del Buffered
             String cadena = bf.readLine();
+            //Mostrem per pantalla la cadena de text
             System.out.println(cadena);
-
+            //Mostrem la cadena + un text adicional que hem ficat nosaltres (I ho escribim)
             outToClient.writeBytes(cadena + " desde el Server \n");
+            //Mostrem en un Pane la cadena.
             JOptionPane.showMessageDialog(null, cadena);
-
+            
             outToClient.close();
             bf.close();
 
